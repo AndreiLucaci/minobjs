4111b64129c802f8d42401ef25af46ac2a87c28f
Andrei Lucaci <andrei.lucaci@code4nord.com>
2019-12-09 17:46:24 +0200

    Added minobjs code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export interface IObjs {
+  merge(...items: any[]): any;
+
+  isEmpty(obj: any): boolean;
+}
+
+export class Objs implements IObjs {
+  public merge(...items: any[]): any {
+    return items.reduce((x, y) => (Object.assign(x, y), {}));
+  }
+
+  public isEmpty(obj: any): boolean {
+    return obj && typeof obj === 'object' && Object.keys(obj).length === 0;
+  }
+}
